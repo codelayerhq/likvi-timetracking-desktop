51b233639d87ccae9efe5ec4765a45bc5e6f1209
Robin Schreiner <robin.schreiner@googlemail.com>
2020-08-07 18:13:00 +0200

    Add tailwind

--- src/background.ts
+++ src/background.ts
@@ -11,7 +11,7 @@ let win: BrowserWindow | null;
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: "app", privileges: { secure: true, standard: true } }
+  { scheme: "app", privileges: { secure: true, standard: true } },
 ]);
 
 function createWindow() {
@@ -23,8 +23,8 @@ function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: (process.env
-        .ELECTRON_NODE_INTEGRATION as unknown) as boolean
-    }
+        .ELECTRON_NODE_INTEGRATION as unknown) as boolean,
+    },
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
@@ -77,7 +77,7 @@ app.on("ready", async () => {
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === "win32") {
-    process.on("message", data => {
+    process.on("message", (data) => {
       if (data === "graceful-exit") {
         app.quit();
       }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./styles/tailwind.css";
 
 createApp(App)
   .use(store)
