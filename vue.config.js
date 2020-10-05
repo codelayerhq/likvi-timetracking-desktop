e7ceac8cea63349506a0499ba4a55059b722770f
Robin Schreiner <robin.schreiner@googlemail.com>
2020-10-05 12:47:35 +0200

    Prepare for mac code signing and notarizing

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,11 @@ module.exports = {
       builderOptions: {
         productName: "likvi Zeiterfassung",
         appId: "de.likvi.timetracking",
+        afterSign: "electron-builder-notarize",
         mac: {
+          hardenedRuntime: true,
+          entitlements: "build/entitlements.mac.plist",
+          entitlementsInherit: "build/entitlements.mac.plist",
           category: "public.app-category.productivit",
         },
       },
